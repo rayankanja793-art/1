14e3a6545fd14d017d2f75476faf74809659c560
rayankanja793-art <rayankanja793@gmail.com>
2026-06-11 12:03:13 +0300

    Create teams.py

---
+++ teams.py
@@ -0,0 +1,9 @@
+# teams.py
+SERIE_A_TEAMS = {
+    "roma": {"id": 1, "color_profile": "yellow_red"},
+    "juventus": {"id": 2, "color_profile": "black_white"},
+    "milan_reds": {"id": 3, "color_profile": "red_black"},
+    "torino": {"id": 4, "color_profile": "maroon"},
+    "fiorentina": {"id": 5, "color_profile": "purple"},
+    # ... add the rest of your list here
+}
